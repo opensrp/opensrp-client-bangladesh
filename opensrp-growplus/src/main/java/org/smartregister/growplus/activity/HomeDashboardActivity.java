df80d2ef66aaddc10fd5db4857d3fd51e356d84e
raihan-mpower <raihan@mpower-social.com>
2018-07-31 05:58:47 +0600

    RA| added provider credentials

--- opensrp-growplus/src/main/java/org/smartregister/growplus/activity/HomeDashboardActivity.java
+++ opensrp-growplus/src/main/java/org/smartregister/growplus/activity/HomeDashboardActivity.java
@@ -2,14 +2,18 @@ package org.smartregister.growplus.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import org.smartregister.growplus.R;
+import org.smartregister.growplus.application.VaccinatorApplication;
 
 public class HomeDashboardActivity extends AppCompatActivity {
 
@@ -59,6 +63,42 @@ public class HomeDashboardActivity extends AppCompatActivity {
             }
         });
 
+        TextView initialsTV = (TextView) findViewById(R.id.name_inits);
+        String preferredName = VaccinatorApplication.getInstance().context().allSharedPreferences().getANMPreferredName(
+                VaccinatorApplication.getInstance().context().allSharedPreferences().fetchRegisteredANM());
+        if (!TextUtils.isEmpty(preferredName)) {
+            String[] initialsArray = preferredName.split(" ");
+            String initials = "";
+            if (initialsArray.length > 0) {
+                initials = initialsArray[0].substring(0, 1);
+                if (initialsArray.length > 1) {
+                    initials = initials + initialsArray[1].substring(0, 1);
+                }
+            }
+
+            initialsTV.setText(initials.toUpperCase());
+        }
+
+        TextView nameTV = (TextView) findViewById(R.id.provider_name);
+        nameTV.setText(capitalize(preferredName));
+
+    }
+
+    public static String capitalize(@NonNull String input) {
+
+        String[] words = input.toLowerCase().split(" ");
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+
+            if (i > 0 && word.length() > 0) {
+                builder.append(" ");
+            }
+
+            String cap = word.substring(0, 1).toUpperCase() + word.substring(1);
+            builder.append(cap);
+        }
+        return builder.toString();
     }
 
 }
