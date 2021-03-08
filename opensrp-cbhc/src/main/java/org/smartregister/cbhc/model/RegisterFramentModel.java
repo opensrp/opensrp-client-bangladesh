7e72d95d90aa14dc68a16b0c1082519458ec84e6
rotnocse@gmail.com <17@jucse44>
2021-03-08 14:31:30 +0600

    performane imrovement with cemed.

--- opensrp-cbhc/src/main/java/org/smartregister/cbhc/model/RegisterFramentModel.java
+++ opensrp-cbhc/src/main/java/org/smartregister/cbhc/model/RegisterFramentModel.java
@@ -77,7 +77,11 @@ public class RegisterFramentModel implements RegisterFragmentContract.Model {
                 tableName + "." + DBConstants.KEY.DOB,
                 tableName + "." + "Patient_Identifier",
                 tableName + "." + DBConstants.KEY.PHONE_NUMBER,
-                "(select ec_details.value from ec_details where ec_details.key='address7' and ec_details.base_entity_id=ec_household.id) as para"};
+                "(select person_address from (select woman.person_address from ec_woman as woman where (woman.person_address = 'address7' and woman.base_entity_id=ec_household.id)"+
+                " " + "Union all  Select member.person_address from ec_member as member WHERE (member.person_address = 'address7' and member.base_entity_id=ec_household.id)" +
+                        " " +
+                        "Union all Select child.person_address from ec_child as child WHERE (child.person_address = 'address7' and child.base_entity_id=ec_household.id))) as para"};
+//                "(select ec_details.value from ec_details where ec_details.key='address7' and ec_details.base_entity_id=ec_household.id) as para"};
         queryBUilder.SelectInitiateMainTable(tableName, columns);
         return queryBUilder.mainCondition(mainCondition);
     }
