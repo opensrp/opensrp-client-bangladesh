ed229a97c6af60ff26c79c0451a7206683622ee8
mahmud6390 <mahmud6390@gmail.com>
2021-12-21 16:15:20 +0600

    jica and gmp flavor

---
+++ opensrp-cbhc/src/main/java/org/smartregister/cbhc/application/AncApplication.java
@@ -0,0 +1,420 @@
+package org.smartregister.cbhc.application;
+
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.evernote.android.job.JobManager;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+import org.smartregister.Context;
+import org.smartregister.CoreLibrary;
+import org.smartregister.cbhc.BuildConfig;
+import org.smartregister.cbhc.activity.LoginActivity;
+import org.smartregister.cbhc.event.TriggerSyncEvent;
+import org.smartregister.cbhc.event.ViewConfigurationSyncCompleteEvent;
+import org.smartregister.cbhc.helper.ECSyncHelper;
+import org.smartregister.cbhc.job.AncJobCreator;
+import org.smartregister.cbhc.job.ViewConfigurationsServiceJob;
+import org.smartregister.cbhc.job.ZScoreRefreshServiceJob;
+import org.smartregister.cbhc.receiver.SyncStatusBroadcastReceiver;
+import org.smartregister.cbhc.repository.AncRepository;
+import org.smartregister.cbhc.repository.HealthIdRepository;
+import org.smartregister.cbhc.repository.UniqueIdRepository;
+import org.smartregister.cbhc.service.intent.PullHealthIdsIntentService;
+import org.smartregister.cbhc.service.intent.PullUniqueIdsIntentService;
+import org.smartregister.cbhc.sync.AncClientProcessorForJava;
+import org.smartregister.cbhc.sync.AncSyncConfiguration;
+import org.smartregister.cbhc.util.Constants;
+import org.smartregister.cbhc.util.DBConstants;
+import org.smartregister.cbhc.util.Utils;
+import org.smartregister.commonregistry.CommonFtsObject;
+import org.smartregister.configurableviews.ConfigurableViewsLibrary;
+import org.smartregister.configurableviews.helper.ConfigurableViewsHelper;
+import org.smartregister.configurableviews.helper.JsonSpecHelper;
+import org.smartregister.configurableviews.repository.ConfigurableViewsRepository;
+import org.smartregister.configurableviews.service.PullConfigurableViewsIntentService;
+import org.smartregister.growthmonitoring.GrowthMonitoringLibrary;
+import org.smartregister.growthmonitoring.repository.WeightRepository;
+import org.smartregister.growthmonitoring.service.intent.ZScoreRefreshIntentService;
+import org.smartregister.immunization.ImmunizationLibrary;
+import org.smartregister.immunization.domain.VaccineSchedule;
+import org.smartregister.immunization.domain.jsonmapping.Vaccine;
+import org.smartregister.immunization.domain.jsonmapping.VaccineGroup;
+import org.smartregister.immunization.util.VaccinatorUtils;
+import org.smartregister.location.helper.LocationHelper;
+import org.smartregister.repository.AllSharedPreferences;
+import org.smartregister.repository.EventClientRepository;
+import org.smartregister.repository.Repository;
+import org.smartregister.sync.ClientProcessorForJava;
+import org.smartregister.sync.DrishtiSyncScheduler;
+import org.smartregister.view.activity.DrishtiApplication;
+import org.smartregister.view.receiver.TimeChangedBroadcastReceiver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import id.zelory.compressor.Compressor;
+
+import static org.smartregister.util.Log.logError;
+import static org.smartregister.util.Log.logInfo;
+
+/**
+ * Created by ndegwamartin on 21/06/2018.
+ */
+public class AncApplication extends DrishtiApplication implements TimeChangedBroadcastReceiver.OnTimeChangedListener {
+
+
+    public static final ArrayList<String> ALLOWED_LEVELS;
+    public static final String DEFAULT_LOCATION_LEVEL = "Health Facility";
+    public static final String FACILITY = "Dispensary";
+    private static final String TAG = AncApplication.class.getCanonicalName();
+    private static JsonSpecHelper jsonSpecHelper;
+    private static CommonFtsObject commonFtsObject;
+
+    static {
+        ALLOWED_LEVELS = new ArrayList<>();
+        ALLOWED_LEVELS.add(DEFAULT_LOCATION_LEVEL);
+        ALLOWED_LEVELS.add(FACILITY);
+    }
+
+    private ConfigurableViewsRepository configurableViewsRepository;
+    private EventClientRepository eventClientRepository;
+    private ConfigurableViewsHelper configurableViewsHelper;
+    private UniqueIdRepository uniqueIdRepository;
+    private HealthIdRepository healthIdRepository;
+    private ECSyncHelper ecSyncHelper;
+    private Compressor compressor;
+    private AncClientProcessorForJava clientProcessorForJava;
+    private String password;
+    private String DEFAULT_PASSWORD = "1e815e13-f6ca-42ef-97c8-83394c201a47";
+    // This Broadcast Receiver is the handler called whenever an Intent with an action named PullConfigurableViewsIntentService.EVENT_SYNC_COMPLETE is broadcast.
+    private BroadcastReceiver syncCompleteMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(android.content.Context context, Intent intent) {
+            // Retrieve the extra data included in the Intent
+
+            int recordsRetrievedCount = intent.getIntExtra(org.smartregister.configurableviews.util.Constants.INTENT_KEY.SYNC_TOTAL_RECORDS, 0);
+            if (recordsRetrievedCount > 0) {
+                Log.d(TAG, "Total records retrieved " + recordsRetrievedCount);
+            }
+
+            Utils.postEvent(new ViewConfigurationSyncCompleteEvent());
+
+            String lastSyncTime = intent.getStringExtra(org.smartregister.configurableviews.util.Constants.INTENT_KEY.LAST_SYNC_TIME_STRING);
+
+            Utils.writePrefString(context, org.smartregister.configurableviews.util.Constants.INTENT_KEY.LAST_SYNC_TIME_STRING, lastSyncTime);
+
+        }
+    };
+
+    public static synchronized AncApplication getInstance() {
+        return (AncApplication) mInstance;
+    }
+    public WeightRepository weightRepository() {
+        return GrowthMonitoringLibrary.getInstance().weightRepository();
+    }
+    public static JsonSpecHelper getJsonSpecHelper() {
+        return jsonSpecHelper;
+    }
+
+    public static CommonFtsObject createCommonFtsObject() {
+        if (commonFtsObject == null) {
+            commonFtsObject = new CommonFtsObject(getFtsTables());
+            for (String ftsTable : commonFtsObject.getTables()) {
+                commonFtsObject.updateSearchFields(ftsTable, getFtsSearchFields());
+                commonFtsObject.updateSortFields(ftsTable, getFtsSortFields());
+            }
+        }
+        return commonFtsObject;
+    }
+
+    private static String[] getFtsTables() {
+        return new String[]{DBConstants.HOUSEHOLD_TABLE_NAME, DBConstants.WOMAN_TABLE_NAME, DBConstants.MEMBER_TABLE_NAME, DBConstants.CHILD_TABLE_NAME};
+    }
+
+    private static String[] getFtsSearchFields() {
+//        return new String[]{DBConstants.KEY.BASE_ENTITY_ID, DBConstants.KEY.FIRST_NAME, DBConstants.KEY.LAST_NAME, DBConstants.KEY.ANC_ID, DBConstants.KEY.DATE_REMOVED, DBConstants.KEY.PHONE_NUMBER};
+        return new String[]{DBConstants.KEY.FIRST_NAME, DBConstants.KEY.LAST_NAME, DBConstants.KEY.PHONE_NUMBER, DBConstants.KEY.DATE_REMOVED, "person_nid", "person_brid", "person_epi", "person_address"};
+
+    }
+
+    private static String[] getFtsSortFields() {
+        return new String[]{DBConstants.KEY.BASE_ENTITY_ID, DBConstants.KEY.FIRST_NAME, DBConstants.KEY.LAST_NAME, DBConstants.KEY.LAST_INTERACTED_WITH, DBConstants.KEY.DATE_REMOVED};
+//        return new String[]{DBConstants.KEY.FIRST_NAME, DBConstants.KEY.LAST_NAME, DBConstants.KEY.PHONE_NUMBER,"person_nid","person_brid","person_epi","person_address",DBConstants.KEY.LAST_INTERACTED_WITH};
+    }
+
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+
+        mInstance = this;
+        context = Context.getInstance();
+        context.updateApplicationContext(getApplicationContext());
+        context.updateCommonFtsObject(createCommonFtsObject());
+
+        //Initialize Modules
+        CoreLibrary.init(context, new AncSyncConfiguration(), BuildConfig.BUILD_TIMESTAMP, null);
+        CoreLibrary.getInstance().setEcClientFieldsFile(Constants.EC_CLIENT_FIELDS);
+
+        LocationHelper.init(ALLOWED_LEVELS, DEFAULT_LOCATION_LEVEL);
+
+        SyncStatusBroadcastReceiver.init(this);
+        TimeChangedBroadcastReceiver.init(this);
+        TimeChangedBroadcastReceiver.getInstance().addOnTimeChangedListener(this);
+
+        JobManager.create(this).addJobCreator(new AncJobCreator());
+        try {
+            Utils.saveLanguage("en");
+        } catch (Exception e) {
+            Utils.appendLog(getClass().getName(), e);
+            Log.e(TAG, e.getMessage());
+        }
+        initLibraries();
+        initOfflineSchedules();
+// Initialize JsonSpec HelperinitLibraries();
+        jsonSpecHelper = new JsonSpecHelper(this);
+
+        setUpEventHandling();
+        String groupId = getPassword();
+//        if (groupId != null && !groupId.isEmpty()) {
+//            initLibraries();
+//            initOfflineSchedules();
+//        }
+
+
+    }
+
+    public void initLibraries() {
+        startZscoreRefreshService();
+        ConfigurableViewsLibrary.init(context, getRepository());
+        ImmunizationLibrary.init(context, getRepository(), null, BuildConfig.VERSION_CODE, BuildConfig.DATABASE_VERSION);
+
+        GrowthMonitoringLibrary.init(context, getRepository(), BuildConfig.VERSION_CODE, BuildConfig.DATABASE_VERSION);
+
+        startPullConfigurableViewsIntentService(this);
+    }
+
+    private AllSharedPreferences getSharedPreferences() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        AllSharedPreferences allSharedPreferences = new AllSharedPreferences(preferences);
+        return allSharedPreferences;
+    }
+
+    public void initOfflineSchedules() {
+        try {
+            List<VaccineGroup> childVaccines = VaccinatorUtils.getSupportedVaccines(this);
+            List<VaccineGroup> womanVaccines = VaccinatorUtils.getSupportedWomanVaccines(this);
+            List<Vaccine> specialVaccines = VaccinatorUtils.getSpecialVaccines(this);
+            VaccineSchedule.init(childVaccines, specialVaccines, "child");
+            VaccineSchedule.init(womanVaccines, null, "woman");
+
+        } catch (Exception e) {
+            Utils.appendLog(getClass().getName(), e);
+            Log.e(TAG, Log.getStackTraceString(e));
+        }
+    }
+
+    @Override
+    public Repository getRepository() {
+        try {
+            if (repository == null) {
+                repository = new AncRepository(getInstance().getApplicationContext(), context);
+                getConfigurableViewsRepository();
+
+            }
+        } catch (UnsatisfiedLinkError e) {
+            logError("Error on getRepository: " + e);
+
+        }
+        return repository;
+    }
+
+    public String getPassword() {
+        if (password == null) {
+            String username = getContext().userService().getAllSharedPreferences().fetchRegisteredANM();
+            password = getContext().userService().getGroupId(username);
+        }
+//        return "";
+        return password;
+
+    }
+
+    @Override
+    public void logoutCurrentUser() {
+
+    }
+    public void logoutUser(){
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        getApplicationContext().startActivity(intent);
+        context.userService().logoutSession();
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    protected void cleanUpSyncState() {
+        try {
+            DrishtiSyncScheduler.stop(getApplicationContext());
+            context.allSharedPreferences().saveIsSyncInProgress(false);
+        } catch (Exception e) {
+            Utils.appendLog(getClass().getName(), e);
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    @Override
+    public void onTerminate() {
+        logInfo("Application is terminating. Stopping Sync scheduler and resetting isSyncInProgress setting.");
+        cleanUpSyncState();
+        TimeChangedBroadcastReceiver.destroy(this);
+        super.onTerminate();
+    }
+
+    public void startPullConfigurableViewsIntentService(android.content.Context context) {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            context.startForegroundService(new Intent(context, PullConfigurableViewsIntentService.class));
+//        } else {
+//            context.startService(new Intent(context, PullConfigurableViewsIntentService.class));
+//        }
+        ViewConfigurationsServiceJob.scheduleJobImmediately(ViewConfigurationsServiceJob.TAG);
+    }
+
+    public ConfigurableViewsRepository getConfigurableViewsRepository() {
+        if (configurableViewsRepository == null)
+            configurableViewsRepository = new ConfigurableViewsRepository(getRepository());
+        return configurableViewsRepository;
+    }
+
+    public EventClientRepository getEventClientRepository() {
+        if (eventClientRepository == null) {
+            eventClientRepository = new EventClientRepository(getRepository());
+        }
+        return eventClientRepository;
+    }
+
+    public UniqueIdRepository getUniqueIdRepository() {
+        if (uniqueIdRepository == null) {
+            uniqueIdRepository = new UniqueIdRepository((AncRepository) getRepository());
+        }
+        return uniqueIdRepository;
+    }
+
+    public HealthIdRepository getHealthIdRepository() {
+        if (healthIdRepository == null) {
+            healthIdRepository = new HealthIdRepository((AncRepository) getRepository());
+        }
+        return healthIdRepository;
+    }
+
+    public ConfigurableViewsHelper getConfigurableViewsHelper() {
+        if (configurableViewsHelper == null) {
+            configurableViewsHelper = new ConfigurableViewsHelper(getConfigurableViewsRepository(),
+                    getJsonSpecHelper(), getApplicationContext());
+        }
+        return configurableViewsHelper;
+    }
+
+    public ECSyncHelper getEcSyncHelper() {
+        if (ecSyncHelper == null) {
+            ecSyncHelper = ECSyncHelper.getInstance(getApplicationContext());
+        }
+        return ecSyncHelper;
+    }
+
+    public Compressor getCompressor() {
+        if (compressor == null) {
+            compressor = Compressor.getDefault(getApplicationContext());
+        }
+        return compressor;
+    }
+
+    public AncClientProcessorForJava getClientProcessorForJava() {
+        if (clientProcessorForJava == null) {
+            clientProcessorForJava = /*ClientProcessorForJava.getInstance(getApplicationContext())*/AncClientProcessorForJava.getInstance(getApplicationContext());
+        }
+        return clientProcessorForJava;
+    }
+
+    private void setUpEventHandling() {
+
+        try {
+
+            LocalBroadcastManager.getInstance(this).registerReceiver(syncCompleteMessageReceiver, new IntentFilter(PullConfigurableViewsIntentService.EVENT_SYNC_COMPLETE));
+
+        } catch
+        (Exception e) {
+            Utils.appendLog(getClass().getName(), e);
+            Log.e(TAG, e.getMessage());
+        }
+
+        EventBus.getDefault().register(this);
+
+
+    }
+
+    @Subscribe(threadMode = ThreadMode.POSTING)
+    public void triggerSync(TriggerSyncEvent event) {
+        if (event != null) {
+            startPullConfigurableViewsIntentService(this);
+            //startSyncService(); call to trigger sync
+        }
+
+    }
+
+    public void startPullHealthIdsService() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            getApplicationContext().startForegroundService(new Intent(getApplicationContext(), PullHealthIdsIntentService.class));
+        } else {
+            getApplicationContext().startService(new Intent(getApplicationContext(), PullHealthIdsIntentService.class));
+        }
+    }
+
+    public void startPullUniqueIdsService() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            getApplicationContext().startForegroundService(new Intent(getApplicationContext(), PullUniqueIdsIntentService.class));
+        } else {
+            getApplicationContext().startService(new Intent(getApplicationContext(), PullUniqueIdsIntentService.class));
+        }
+    }
+
+    @Override
+    public void onTimeChanged() {
+//        Utils.showToast(this, this.getString(R.string.device_time_changed));
+//        context.userService().forceRemoteLogin();
+//        logoutCurrentUser();
+    }
+
+    @Override
+    public void onTimeZoneChanged() {
+//        Utils.showToast(this, this.getString(R.string.device_timezone_changed));
+//        context.userService().forceRemoteLogin();
+//        logoutCurrentUser();
+    }
+
+
+    public void startZscoreRefreshService() {
+        ZScoreRefreshServiceJob.scheduleJobImmediately(ZScoreRefreshServiceJob.TAG);
+//        Intent intent = new Intent(getInstance().getApplicationContext(), ZScoreRefreshIntentService.class);
+//        this.getApplicationContext().startService(intent);
+    }
+    public void forcelogoutCurrentUser() {
+        getContext().userService().getAllSharedPreferences().saveForceRemoteLogin(true);
+        org.smartregister.cbhc.helper.LocationHelper.setInstance(null);
+        logoutCurrentUser();
+        System.exit(0);
+    }
+}
---
+++ opensrp-cbhc/src/main/java/org/smartregister/cbhc/fragment/RecordMUACDialogFragment.java
@@ -0,0 +1,226 @@
+package org.smartregister.cbhc.fragment;//package org.smartregister.cbhc.fragment;
+//
+//import android.app.Activity;
+//import android.app.DialogFragment;
+//import android.content.Context;
+//import android.os.Bundle;
+//import android.os.Handler;
+//import android.text.Selection;
+//import android.util.Pair;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.view.Window;
+//import android.view.inputmethod.InputMethodManager;
+//import android.widget.Button;
+//import android.widget.DatePicker;
+//import android.widget.EditText;
+//import android.widget.FrameLayout;
+//import android.widget.TextView;
+//
+//import org.apache.commons.lang3.StringUtils;
+//import org.joda.time.DateTime;
+//import org.smartregister.cbhc.R;
+//import org.smartregister.cbhc.util.GrowthUtil;
+//import org.smartregister.growthmonitoring.fragment.GrowthDialogFragment;
+//import org.smartregister.util.DatePickerUtils;
+//import org.smartregister.util.DateUtil;
+//import org.smartregister.util.Utils;
+//import org.smartregister.view.customcontrols.CustomFontTextView;
+//
+//import java.util.Calendar;
+//import java.util.Date;
+//import java.util.HashMap;
+//import java.util.Random;
+//
+//import static org.smartregister.util.Utils.getName;
+//import static org.smartregister.util.Utils.getValue;
+//
+//public class RecordMUACDialogFragment extends GrowthDialogFragment {
+//
+//
+//    private Date dateOfBirth;
+//
+//    public static final String WRAPPER_TAG = "tag";
+//    public static final String DATE_OF_BIRTH_TAG = "dob";
+//    public static final HashMap<Long, Pair<String, String>>muacmap = new HashMap<>();
+//
+//    class MUAC{
+//        String duration;
+//        String muac;
+//    }
+//
+//    public static RecordMUACDialogFragment newInstance() {
+//        RecordMUACDialogFragment recordMUACDialogFragment = new RecordMUACDialogFragment();
+//        return recordMUACDialogFragment;
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setStyle(DialogFragment.STYLE_NO_TITLE, android.R.style.Theme_Holo_Light_Dialog);
+//    }
+//
+//    @Override
+//    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
+//                             Bundle savedInstanceState) {
+//
+//        dateOfBirth = GrowthUtil.getDateOfBirth();
+//
+//        ViewGroup dialogView = (ViewGroup) inflater.inflate(R.layout.record_muac_dialog_view, container, false);
+//
+//        final EditText editMUAC = (EditText) dialogView.findViewById(org.smartregister.growthmonitoring.R.id.edit_weight);
+//        CustomFontTextView titleView = dialogView.findViewById(org.smartregister.growthmonitoring.R.id.record_weight);
+//        TextView value_unit_symbol = dialogView.findViewById(org.smartregister.growthmonitoring.R.id.value_unit_symbol);
+//        value_unit_symbol.setText("cm");
+//        titleView.setText("Record MUAC");
+//        //formatEditMUACView(editMUAC, "");
+//
+//        final DatePicker earlierDatePicker = (DatePicker) dialogView.findViewById(org.smartregister.growthmonitoring.R.id.earlier_date_picker);
+//        earlierDatePicker.setMaxDate(Calendar.getInstance().getTimeInMillis());
+//        if (dateOfBirth != null) {
+//            earlierDatePicker.setMinDate(dateOfBirth.getTime());
+//        }
+//
+//        String firstName = org.smartregister.util.Utils.getValue(GrowthUtil.childDetails.getColumnmaps(), "first_name", true);
+//        String lastName = Utils.getValue(GrowthUtil.childDetails.getColumnmaps(), "last_name", true);
+//        String childName = getName(firstName, lastName).trim();
+//        TextView nameView = (TextView) dialogView.findViewById(org.smartregister.growthmonitoring.R.id.child_name);
+//        nameView.setText(childName);
+//
+//        String zeirId = getValue(GrowthUtil.childDetails.getColumnmaps(), "zeir_id", false);
+//        TextView numberView = (TextView) dialogView.findViewById(org.smartregister.growthmonitoring.R.id.child_zeir_id);
+//        numberView.setText(String.format("%s: %s", getString(org.smartregister.growthmonitoring.R.string.label_zeir), zeirId));
+//
+//        String duration = "";
+//        String dobString = getValue(GrowthUtil.childDetails.getColumnmaps(), "dob", false);
+//        if (StringUtils.isNotBlank(dobString)) {
+//            DateTime dateTime = new DateTime(getValue(GrowthUtil.childDetails.getColumnmaps(), "dob", false));
+//            duration = DateUtil.getDuration(dateTime);
+//        }
+//        TextView ageView = (TextView) dialogView.findViewById(org.smartregister.growthmonitoring.R.id.child_age);
+//        if (StringUtils.isNotBlank(duration)) {
+//            ageView.setText(String.format("%s: %s", getString(org.smartregister.growthmonitoring.R.string.age), duration));
+//        } else {
+//            ageView.setText("");
+//        }
+//
+//
+//        final Button set = (Button) dialogView.findViewById(org.smartregister.growthmonitoring.R.id.set);
+//        set.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String heightString = editMUAC.getText().toString();
+//                if (StringUtils.isBlank(heightString) || Float.valueOf(heightString) <= 0f) {
+//                    return;
+//                }
+//
+//                dismiss();
+//
+//                int day = earlierDatePicker.getDayOfMonth();
+//                int month = earlierDatePicker.getMonth();
+//                int year = earlierDatePicker.getYear();
+//
+//                Calendar calendar = Calendar.getInstance();
+//                calendar.set(year, month, day);
+//
+//
+//
+//            }
+//        });
+//
+//        final Button heightTakenToday = (Button) dialogView.findViewById(org.smartregister.growthmonitoring.R.id.weight_taken_today);
+//        heightTakenToday.setText("MUAC taken today");
+//        heightTakenToday.setOnClickListener(new Button.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String heightString = editMUAC.getText().toString();
+//                if (StringUtils.isBlank(heightString) || Float.valueOf(heightString) <= 0f) {
+//                    return;
+//                }
+//
+//                dismiss();
+//
+//                Calendar calendar = Calendar.getInstance();
+//                Float height = Float.valueOf(heightString);
+////                muacmap.put(muacmap.size()+1,Pair.create())
+//
+//            }
+//        });
+//
+//        final Button heightTakenEarlier = (Button) dialogView.findViewById(org.smartregister.growthmonitoring.R.id.weight_taken_earlier);
+//        heightTakenEarlier.setText("MUAC taken earlier");
+//        heightTakenEarlier.setOnClickListener(new Button.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                heightTakenEarlier.setVisibility(View.GONE);
+//
+//                InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+//                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+//
+//                earlierDatePicker.setVisibility(View.VISIBLE);
+//                earlierDatePicker.requestFocus();
+//                set.setVisibility(View.VISIBLE);
+//
+//                DatePickerUtils.themeDatePicker(earlierDatePicker, new char[]{'d', 'm', 'y'});
+//            }
+//        });
+//
+//        Button cancel = (Button) dialogView.findViewById(org.smartregister.growthmonitoring.R.id.cancel);
+//        cancel.setOnClickListener(new Button.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                dismiss();
+//            }
+//        });
+//
+//        return dialogView;
+//    }
+//
+//    @Override
+//    public void onAttach(Activity activity) {
+//        super.onAttach(activity);
+//        // Verify that the host activity implements the callback interface
+//
+//    }
+//
+//    private void formatEditMUACView(EditText editMUAC, String userInput) {
+//        StringBuilder stringBuilder = new StringBuilder(userInput);
+//
+//        while (stringBuilder.length() > 2 && stringBuilder.charAt(0) == '0') {
+//            stringBuilder.deleteCharAt(0);
+//        }
+//        while (stringBuilder.length() < 2) {
+//            stringBuilder.insert(0, '0');
+//        }
+//        stringBuilder.insert(stringBuilder.length() - 1, '.');
+//
+//        editMUAC.setText(stringBuilder.toString());
+//        // keeps the cursor always to the right
+//        Selection.setSelection(editMUAC.getText(), stringBuilder.toString().length());
+//    }
+//
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // without a handler, the window size itself correctly
+//        // but the keyboard does not show up
+//        new Handler().post(new Runnable() {
+//            @Override
+//            public void run() {
+//                Window window = null;
+//                if (getDialog() != null) {
+//                    window = getDialog().getWindow();
+//                }
+//
+//                if (window == null) {
+//                    return;
+//                }
+//                window.setLayout(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+//
+//            }
+//
+//        });
+//
+//    }
+//}
